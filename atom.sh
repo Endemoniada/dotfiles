38e8420f009cc91290bd903b8c3ca8fb56995f46
Martin Ekberg <martin@mekberg.se>
2019-12-03 12:22:55 +0100

    Reflow commands

--- atom.sh
+++ atom.sh
@@ -2,43 +2,48 @@
 set -e
 
 echo "Installing Atom themes..."
-apm install atom-material-syntax \
- atom-material-syntax-dark \
- base16-eighties-dark \
- base16-eighties-one-dark \
- nord-atom-syntax \
- nord-atom-ui \
- tomorrow-night-eighties
+apm install \
+atom-material-syntax \
+atom-material-syntax-dark \
+base16-eighties-dark \
+base16-eighties-one-dark \
+nord-atom-syntax \
+nord-atom-ui \
+tomorrow-night-eighties \
 
+echo
 echo "Installing Atom packages..."
-apm install Sublime-Style-Column-Selection \
- ansible-vault \
- atom-csv-markdown \
- atom-overtype-mode \
- busy-signal \
- docblock-python \
- file-icons \
- git-plus \
- highlight-selected \
- intentions \
- language-diff \
- language-generic-config \
- language-powershell \
- language-rpm-spec \
- line-length-break \
- linter \
- linter-flake8 \
- linter-shellcheck \
- linter-ui-default \
- minimap \
- multi-wrap-guide \
- multifile-rename \
- python-autopep8 \
- python-black \
- python-tools \
- rulerz \
- script \
- sort-lines \
- ssh-config \
- tablr \
- trailing-spaces
+apm install \
+ansible-vault \
+atom-csv-markdown \
+atom-overtype-mode \
+autocomplete-python \
+busy-signal \
+docblock-python \
+file-icons \
+git-plus \
+highlight-selected \
+intentions \
+language-diff \
+language-generic-config \
+language-powershell \
+language-rpm-spec \
+line-length-break \
+linter \
+linter-flake8 \
+linter-shellcheck \
+linter-ui-default \
+markdown-preview-plus \
+minimap \
+multi-wrap-guide \
+multifile-rename \
+python-autopep8 \
+python-black \
+python-tools \
+rulerz \
+script \
+sort-lines \
+ssh-config \
+Sublime-Style-Column-Selection \
+tablr \
+trailing-spaces \
